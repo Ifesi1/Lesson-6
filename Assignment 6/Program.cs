dc9336873665b6fd5775ae5b27015a27a97b254f
IFESINACHI <IFESINACHI@DESKTOP-KFCG7TC>
2022-03-28 00:01:41 +0100

    Add project files.

---
+++ Assignment 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Assignment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uisng this formula below, input your values accordingly: ");
+            Console.WriteLine("A^2 + BX + C = 0");
+
+            Console.Write("Please enter a value for A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a value for B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a value for C: ");
+            int C = int.Parse(Console.ReadLine());
+
+            if ((Math.Pow(B,2))-(4*A*C)>0)
+            {
+                Console.WriteLine("Real roots");
+            }
+            else if ((Math.Pow(B, 2)) - (4 * A * C) < 0)
+            {
+                Console.WriteLine("Complex Roots");
+            }
+            else
+            {
+                Console.WriteLine("Equal Roots");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson 6 - Conditioning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_6___Conditioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The given quadratic equation is: ");
+            Console.WriteLine("X^2 + 6X + 5 = 0");
+            int a = 1;
+            int b = 6;
+            int c = 5;
+
+            if ((Math.Pow(b,2))-(4*a*c)>0)
+            {
+                Console.WriteLine("Real roots");
+            }
+            else if ((Math.Pow(b, 2)) - (4 * a * c) < 0)
+            {
+                Console.WriteLine("Complex Roots");
+            }
+            else
+            {
+                Console.WriteLine("Equal Roots");
+            }
+            Console.WriteLine();
+        }
+    }
+}
